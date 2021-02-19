8dfde0224a8a956a89f149a7538bf9fbf71455c4
Fachri <fachrighiffary@gmail.com>
2021-02-19 13:34:34 +0700

    fix some bug

--- src/configs/mySQL.js
+++ src/configs/mySQL.js
@@ -1,15 +1,10 @@
 const mysql = require('mysql');
-const {HOST, DB, USER, PASS} = process.env;
-
-// console.log(HOST)
-// console.log(DB)
-// console.log(USER)
-// console.log(PASS)
+const {HOST, DB, USER, PASS, DB_USER, DB_PASS} = process.env;
 
 const db = mysql.createConnection({
     host: HOST,
-    user: USER,
-    password: PASS,
+    user: DB_USER,
+    password: DB_PASS,
     database: DB
 })
 
