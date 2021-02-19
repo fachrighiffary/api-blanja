2aebbfd5da6aa0b3c395ab65730e7594ec2d4153
Fachri <fachrighiffary@gmail.com>
2021-02-19 11:39:14 +0700

    fix some bug

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,18 +2,11 @@ const express = require("express");
 
 const productsRouter = express.Router();
 const checkToken = require("../helpers/middleware/checkToken")
-
-// const db = require("../configs/mySQL");
-
 const productsController = require("../controllers/products");
 const multipleUpload = require("../helpers/middleware/upload");
 
-//localhost:8000/products
 productsRouter.get("/", productsController.getAllProducts);
-
-//localhost:8000/products => method post
 productsRouter.post("/",checkToken.login,  checkToken.seller, multipleUpload, productsController.createProducts);
-
 productsRouter.get("/:id", checkToken.login, productsController.getProductByUser);
 
 
